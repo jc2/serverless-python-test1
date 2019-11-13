8e3d460ce150489c9c4bd6116f284ac82e01508e
Juan Ceron <Juan.Ceron@natgeo.com>
2019-11-13 18:23:18 -0500

    first step

--- handler.py
+++ handler.py
@@ -1,24 +1,22 @@
 import json
+import requests
 
-
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
+def main(event, context):
+    r = requests.get("https://www.google.com")
+    if r.status_code == 200:
+        response = {
+            'statusCode': 200,
+            'body': json.dumps('Hello from Google!'),
+            # 'event': json.dumps(event),
+            # 'context_func_name': json.dumps(context.function_name)
+        }
+    else:
+        response = {
+            'statusCode': 500,
+            'body': json.dumps('Something went wrong!'),
+        }
+    print(response)
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+if __name__ == "__main__":
+    main('', '')
